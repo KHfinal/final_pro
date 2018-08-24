4b6af1f949d58f8c1b4c6575d668fe3f2510cdf5
Gi-rit <40787528+Gi-rit@users.noreply.github.com>
2018-08-24 20:45:51 +0900

    gitignore파일추가

--- jarvis/src/main/java/kh/mark/jarvis/friend/controller/FriendController.java
+++ jarvis/src/main/java/kh/mark/jarvis/friend/controller/FriendController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import kh.mark.jarvis.friend.model.service.FriendService;
 import kh.mark.jarvis.friend.model.vo.Friend;
-import kh.mark.jarvis.member.model.vo.member.Member;
 
 @Controller
 public class FriendController{
