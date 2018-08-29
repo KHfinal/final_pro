f35ee701010a5e7e22945a7c89dd781a1d5ec642
hoho1234567890 <41193804+hoho1234567890@users.noreply.github.com>
2018-08-29 21:31:35 +0900

    2018-08-29 9:30

---
+++ jarvis/src/main/java/kh/mark/jarvis/common/StringArrayType.java
@@ -0,0 +1,46 @@
+package kh.mark.jarvis.common;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
+public class StringArrayType implements TypeHandler<String[]> {
+
+	@Override
+	public void setParameter(PreparedStatement ps, int i, String[] parameter, JdbcType jdbcType) throws SQLException {
+		if(parameter!=null) {
+			ps.setString(i, String.join(",", parameter));
+		}
+		else {
+			ps.setString(i, "");
+		}
+		
+	}
+
+	@Override
+	public String[] getResult(ResultSet rs, String columnName) throws SQLException {
+		String columnValue=rs.getString(columnName);
+		String[] columnArray=columnValue.split(",");
+		return columnArray;
+	}
+
+	@Override
+	public String[] getResult(ResultSet rs, int columnIndex) throws SQLException {
+		String columnValue=rs.getString(columnIndex);
+		String[] columnArray=columnValue.split(",");
+		return columnArray;
+	}
+
+	@Override
+	public String[] getResult(CallableStatement cs, int columnIndex) throws SQLException {
+		String columnValue=cs.getString(columnIndex);
+		String[] columnArray=columnValue.split(",");
+		return columnArray;
+	}
+
+	
+}
---
+++ jarvis/src/main/webapp/WEB-INF/views/group/groupView.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
+<%@ taglib prefix='fmt' uri='http://java.sun.com/jsp/jstl/fmt'%>
+<%@ taglib prefix='fn' uri='http://java.sun.com/jsp/jstl/functions'%>
+<c:set var="path" value="<%=request.getContextPath()%>"/>
+<jsp:include page="/WEB-INF/views/common/header.jsp">
+	<jsp:param value="social" name="title"/>	
+</jsp:include>
+<title>Agency - Start Bootstrap Theme</title>
+<link href="path/resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="path/resources/bootstrap/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<link href="path/resources/css/agency.min.css" rel="stylesheet">
+
+<div>
+	
+</div>
