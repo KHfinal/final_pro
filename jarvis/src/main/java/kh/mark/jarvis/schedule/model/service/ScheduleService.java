79a5a6db7ee7553ac0cbe7a73683732dfdad521a
Gi-rit <40787528+Gi-rit@users.noreply.github.com>
2018-08-30 21:35:52 +0900

    일정등록 프로토타입 완료

--- jarvis/src/main/java/kh/mark/jarvis/schedule/model/service/ScheduleService.java
+++ jarvis/src/main/java/kh/mark/jarvis/schedule/model/service/ScheduleService.java
@@ -1,5 +1,8 @@
 package kh.mark.jarvis.schedule.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import kh.mark.jarvis.schedule.model.vo.Schedule;
@@ -7,5 +10,7 @@ import kh.mark.jarvis.schedule.model.vo.Schedule;
 public interface ScheduleService {
 
 	int addSchedule(Schedule s);
+
+	List<Map<String, Object>> eventList(String userEmail);
 	
 }
